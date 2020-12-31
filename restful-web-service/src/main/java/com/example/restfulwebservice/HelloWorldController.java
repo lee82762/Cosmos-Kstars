276eb6e19ce7ee5b9b15ca111ca2c6a558f029b6
Leejaebum <cjdrn0208@naver.com>
2020-12-31 20:35:51 +0900

    Restful-Web-Service

---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello-world")
+    public String hewlloWorld(){
+        return "Hello World123";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("helloworld");
+    }
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean1(@PathVariable String name){
+        return new HelloWorldBean(String.format("helloworld, %s",name));
+    }
+
+}
